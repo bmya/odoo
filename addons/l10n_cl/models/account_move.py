99b8223b2ceee08cdeecbaa4d624ef0c048925b7
Susana Vázquez <susana.vazq@gmail.com>
2019-08-10 23:10:48 +0200

    [IMP] Cleaning code

--- addons/l10n_cl/models/account_move.py
+++ addons/l10n_cl/models/account_move.py
@@ -7,13 +7,13 @@ from odoo import fields, models
 class AccountMove(models.Model):
     _inherit = "account.move"
 
+    l10n_latam_document_type_id_code = fields.Char(related='l10n_latam_document_type_id.code', string='Doc Type')
+    partner_id_vat = fields.Char(related='partner_id.vat', string='VAT No')
+
     def get_document_type_sequence(self):
         """ Return the match sequences for the given journal and invoice """
         self.ensure_one()
         if self.journal_id.l10n_latam_use_documents and self.l10n_latam_country_code == 'CL':
-            if self.journal_id.l10n_cl_share_sequences:
-                return self.journal_id.l10n_cl_sequence_ids.filtered(
-                    lambda x: x.l10n_cl_letter == self.l10n_latam_document_type_id.l10n_cl_letter)
             res = self.journal_id.l10n_cl_sequence_ids.filtered(
                 lambda x: x.l10n_latam_document_type_id == self.l10n_latam_document_type_id)
             return res
@@ -33,23 +33,5 @@ class AccountMove(models.Model):
             # domain = [('id', '=', seq.l10n_latam_document_type_id.id)]
         return domain
 
-    # def _get_account_move_type(self):
-    #     if self.type:
-    #         return self.type
-    #     return self._context.get('default_type')
-    #
-    # def post(self):
-    #     internal_type = self._get_account_move_type()
-    #     for rec in self.filtered(lambda x: x.l10n_latam_use_documents and not x.l10n_latam_document_number and
-    #                              self.journal_id.company_id.country_id == self.env.ref('base.cl') and
-    #                              internal_type in ['out_invoice', 'out_refund']):
-    #         if internal_type == 'out_invoice':
-    #             sequence = self.journal_id.sequence_number_next
-    #         else:
-    #             sequence = self.journal_id.refund_sequence_number_next
-    #         rec.l10n_latam_document_number = sequence
-    #         rec._get_sequence().next_by_id(sequence_date=rec.date)
-    #     return super().post()
 
-    l10n_latam_document_type_id_code = fields.Char(related='l10n_latam_document_type_id.code', string='Doc Type')
-    partner_id_vat = fields.Char(related='partner_id.vat', string='VAT No')
+
